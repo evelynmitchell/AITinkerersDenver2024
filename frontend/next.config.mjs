f704fb18eb9e1192bde090badc41d4bfb4a93306
Akhil <akhile76@gmail.com>
2024-08-03 22:23:30 +0530

    set max body size to 1gb

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "1000mb",
+    },
+  },
+};
 
 export default nextConfig;
